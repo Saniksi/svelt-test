76b49f2581f21f50c94fbabc5da3e70aece0d622
Oleksandr Chyzh <itep@ukr.net>
2023-05-16 21:11:35 +0300

    first-commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  // let name = 'Saniksi';
+  // let src = 'http://picsum.com/200/200';
+  // const string = 'This is <strong>some string</strong>';
+
+  import Counter from './lib/Counter.svelte';
+</script>
+
+<!-- <h1>Hello {name.toUpperCase()}</h1>
+<img {src} alt="This is an image {name}" />
+<p>{@html string}</p> -->
+
+<Counter />
+
+<style>
+  /* h1 {
+    color: red;
+  } */
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,29 @@
+<script>
+  let count = 0;
+  let string;
+  let increment = () => {
+    count += 1;
+  };
+
+  // $: {
+  //   console.log(`The count is ${count}`);
+  // }
+
+  $: if (count > 5) {
+    console.log(`The count is ${count}`);
+  }
+
+  $: string = `You have clicked ${count} times.`
+</script>
+
+<button on:click={increment}>Clicks {count}</button>
+<h3>{string}</h3>
+
+<h5>You clicked {count}</h5>
+
+<style>
+  button {
+    padding: 20px;
+    font-size: 2em;
+  }
+</style>
